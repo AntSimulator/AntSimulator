a0d801a14c9845d5c7f227cb059b8e93d7d0cc78
Minkyoung Lee <minkyoungemilylee@ewha.ac.kr>
2026-01-16 23:15:35 +0900

    Update MarketSimulator.cs

--- AntSimulator/Assets/Scripts/StockMarket/MarketSimulator.cs
+++ AntSimulator/Assets/Scripts/StockMarket/MarketSimulator.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.InputSystem.Controls;
 
 public class MarketSimulator : MonoBehaviour
 {
@@ -10,11 +14,16 @@ public class MarketSimulator : MonoBehaviour
     [Header("Tick")] public float tickIntervalSec = 1.0f;
 
     [Header("Simulation Parameters")] 
-    public float baseDrift = 0.0f; // 기본적인 시장의 방 (양수면 기본적으로 올라감. 음수면 내려감. 물론 방향성인거지 다른 영향으로 drift와 다르게 흘러갈 수 잇음)
+    [Range(-50, 50)] public float baseDrift = 0.0f; // 기본적인 시장의 방 (양수면 기본적으로 올라감. 음수면 내려감. 물론 방향성인거지 다른 영향으로 drift와 다르게 흘러갈 수 잇음)
     public float noiseScale = 1.0f; //가격 변화에 들어갈 랜덤성을 얼마나 크게 할지 정하는 변수. noise가 크면 클수록 예측 불가능성 커짐
+    public float eventScale = 1.0f;
     public float maxUpPercentPerTick = 0.08f;
     public float maxDownPercentPerTick = 0.08f;
     public float minPrice = 1f;
+    
+    
+    //TODO change this variable with actual stock price
+    private float stockPrice = 10.0f;
 
     private float timer = 0f;
 
@@ -43,5 +52,13 @@ public class MarketSimulator : MonoBehaviour
         //TODO: for each stock, save previous price, create logic for stock market
         
         //TODO: log print out changes of each stock per tick
+    }
+
+    // Tick 안에서 얘 호출하면 값 바뀜
+    private void calculateStockPrice()
+    {
+        float rdNum = UnityEngine.Random.Range(-10f, 11f)*noiseScale;
+
+        stockPrice = stockPrice * (1+Mathf.Clamp((baseDrift + rdNum + eventScale), maxDownPercentPerTick, maxUpPercentPerTick));
     }
 }
